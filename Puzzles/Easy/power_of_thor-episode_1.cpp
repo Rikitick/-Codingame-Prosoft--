6c31208d6d5793e6a8ce2d05c7efdf1d34df20c0
Rikitick <100961083+Rikitick@users.noreply.github.com>
2025-09-24 20:26:40 +0500

    Create power_of_thor-episode_1.cpp

---
+++ Puzzles/Easy/power_of_thor-episode_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int light_x; 
+    int light_y; 
+    int initial_tx; 
+    int initial_ty; 
+    cin >> light_x >> light_y >> initial_tx >> initial_ty; cin.ignore();
+
+    while (1) {
+        int remaining_turns; 
+        cin >> remaining_turns; cin.ignore();
+
+        if (initial_tx < light_x && initial_ty < light_y) {
+            cout << "SE" << endl; 
+            initial_tx++; 
+            initial_ty++; 
+        }
+        else if (initial_tx == light_x && initial_ty < light_y) {
+            cout << "S" << endl; 
+            initial_ty++;
+        }
+        else if (initial_tx < light_x && initial_ty == light_y) {
+            cout << "E" << endl;
+            initial_tx++;
+        }
+        else if (initial_tx > light_x && initial_ty > light_y) {
+            cout << "NW" << endl; 
+            initial_tx--; 
+            initial_ty--; 
+        }
+        else if (initial_tx == light_x && initial_ty > light_y) {
+            cout << "N" << endl; 
+            initial_ty--; 
+        }
+        else if (initial_tx > light_x && initial_ty == light_y) {
+            cout << "W" << endl; 
+            initial_tx--; 
+        }
+        else if (initial_tx < light_x && initial_ty > light_y) {
+            cout << "NE" << endl; 
+            initial_tx++; 
+            initial_ty--; 
+        }
+        else if (initial_tx > light_x && initial_ty < light_y) {
+            cout << "SW" << endl; 
+            initial_tx--; 
+            initial_ty++; 
+        }
+    }
+}
