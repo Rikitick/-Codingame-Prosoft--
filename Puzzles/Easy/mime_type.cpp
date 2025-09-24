220b1845cd97eb450b7580760aa44ac0df249b00
Rikitick <100961083+Rikitick@users.noreply.github.com>
2025-09-24 23:15:23 +0500

    Update mime_type.cpp

--- Puzzles/Easy/mime_type.cpp
+++ Puzzles/Easy/mime_type.cpp
@@ -56,4 +56,5 @@ int main()
     }
     cout << endl;
     // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+    return 0;
 }
